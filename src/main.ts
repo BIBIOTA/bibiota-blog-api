9dd1d22a09c1c8e8ec3eabc594079ff547db3b2b
Yuki Ota <yukiotataitien@gmail.com>
2022-11-15 20:00:00 +0800

    feat(upgrade): Upgrade version & Change service to fly.io

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
     allowedHeaders: '*',
     origin: process.env.frontendUrl,
   });
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
--- src/traffic/traffic.service.spec.ts
+++ src/traffic/traffic.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TrafficService } from './traffic.service';
+import { Traffic } from './traffic.model';
+import { getModelToken } from '@nestjs/mongoose';
 
 describe('TrafficService', () => {
   let service: TrafficService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TrafficService],
+      providers: [
+        TrafficService,
+        {
+          provide: getModelToken(Traffic.name),
+          useValue: Traffic,
+        },
+      ],
     }).compile();
 
     service = module.get<TrafficService>(TrafficService);
